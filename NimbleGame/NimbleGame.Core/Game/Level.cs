eb7b02a62591202bde89994901874187593b9694
João Reis <joaocibraoreis@gmail.com>
2024-05-22 23:19:43 +0100

    File upload

---
+++ NimbleGame/NimbleGame.Core/Game/Level.cs
@@ -0,0 +1,429 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using System.IO;
+using Microsoft.Xna.Framework.Input;
+
+namespace NimbleGame
+{
+
+    class Level : IDisposable
+    {
+        private Tile[,] tiles;
+        private Layer[] layers;
+        private const int EntityLayer = 2;
+
+        public Player Player
+        {
+            get { return player; }
+        }
+        Player player;
+
+        private List<Gem> gems = new List<Gem>();
+        private List<Enemy> enemies = new List<Enemy>();
+
+        private Vector2 start;
+        private Point exit = InvalidPosition;
+        private static readonly Point InvalidPosition = new Point(-1, -1);
+
+        private Random random = new Random(354668); 
+        public float cameraPosition;
+
+
+        public int Score
+        {
+            get { return score; }
+        }
+        int score;
+
+        public bool ReachedExit
+        {
+            get { return reachedExit; }
+        }
+        bool reachedExit;
+
+        public TimeSpan TimeRemaining
+        {
+            get { return timeRemaining; }
+        }
+        TimeSpan timeRemaining;
+
+        private const int PointsPerSecond = 5;
+
+        public ContentManager Content
+        {
+            get { return content; }
+        }
+        ContentManager content;
+
+        private SoundEffect exitReachedSound;
+
+        #region Loading
+
+        public Level(IServiceProvider serviceProvider, Stream fileStream, int levelIndex)
+        {
+            content = new ContentManager(serviceProvider, "Content");
+
+            timeRemaining = TimeSpan.FromMinutes(2.0);
+
+            LoadTiles(fileStream);
+
+
+            layers = new Layer[3];
+            layers[0] = new Layer(Content, "Backgrounds/Layer0", 0.2f);
+            layers[1] = new Layer(Content, "Backgrounds/Layer1", 0.5f);
+            layers[2] = new Layer(Content, "Backgrounds/Layer2", 0.8f);
+
+            exitReachedSound = Content.Load<SoundEffect>("Sounds/ExitReached");
+        }
+
+        private void LoadTiles(Stream fileStream)
+        {
+            int width;
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = reader.ReadLine();
+                width = line.Length;
+                while (line != null)
+                {
+                    lines.Add(line);
+                    if (line.Length != width)
+                        throw new Exception(String.Format("The length of line {0} is different from all preceeding lines.", lines.Count));
+                    line = reader.ReadLine();
+                }
+            }
+
+            tiles = new Tile[width, lines.Count];
+
+            for (int y = 0; y < Height; ++y)
+            {
+                for (int x = 0; x < Width; ++x)
+                {
+                    char tileType = lines[y][x];
+                    tiles[x, y] = LoadTile(tileType, x, y);
+                }
+            }
+
+            if (Player == null)
+                throw new NotSupportedException("A level must have a starting point.");
+            if (exit == InvalidPosition)
+                throw new NotSupportedException("A level must have an exit.");
+
+        }
+
+        private Tile LoadTile(char tileType, int x, int y)
+        {
+            switch (tileType)
+            {
+                // Blank space
+                case '.':
+                    return new Tile(null, TileCollision.Passable);
+
+                // Exit
+                case 'X':
+                    return LoadExitTile(x, y);
+
+                // Gem
+                case 'G':
+                    return LoadGemTile(x, y);
+
+                // Floating platform
+                case '-':
+                    return LoadTile("Platform", TileCollision.Platform);
+
+                // Various enemies
+                case 'A':
+                    return LoadEnemyTile(x, y, "MonsterA");
+                case 'B':
+                    return LoadEnemyTile(x, y, "MonsterB");
+                case 'C':
+                    return LoadEnemyTile(x, y, "MonsterC");
+                case 'D':
+                    return LoadEnemyTile(x, y, "MonsterD");
+
+                // Platform block
+                case '~':
+                    return LoadVarietyTile("BlockB", 2, TileCollision.Impassable);
+
+                // Passable block
+                case ':':
+                    return LoadVarietyTile("BlockB", 2, TileCollision.Passable);
+
+                // Player 1 start point
+                case '1':
+                    return LoadStartTile(x, y);
+
+                // Impassable block
+                case '#':
+                    return LoadVarietyTile("BlockA", 7, TileCollision.Impassable);
+
+                // Unknown tile type character
+                default:
+                    throw new NotSupportedException(String.Format("Unsupported tile type character '{0}' at position {1}, {2}.", tileType, x, y));
+            }
+        }
+
+
+        private Tile LoadTile(string name, TileCollision collision)
+        {
+            return new Tile(Content.Load<Texture2D>("Tiles/" + name), collision);
+        }
+
+
+
+        private Tile LoadVarietyTile(string baseName, int variationCount, TileCollision collision)
+        {
+            int index = random.Next(variationCount);
+            return LoadTile(baseName + index, collision);
+        }
+
+
+
+        private Tile LoadStartTile(int x, int y)
+        {
+            if (Player != null)
+                throw new NotSupportedException("A level may only have one starting point.");
+
+            start = RectangleExtensions.GetBottomCenter(GetBounds(x, y));
+            player = new Player(this, start);
+
+            return new Tile(null, TileCollision.Passable);
+        }
+
+
+        private Tile LoadExitTile(int x, int y)
+        {
+            if (exit != InvalidPosition)
+                throw new NotSupportedException("A level may only have one exit.");
+
+            exit = GetBounds(x, y).Center;
+
+            return LoadTile("Exit", TileCollision.Passable);
+        }
+
+
+        private Tile LoadEnemyTile(int x, int y, string spriteSet)
+        {
+            Vector2 position = RectangleExtensions.GetBottomCenter(GetBounds(x, y));
+            enemies.Add(new Enemy(this, position, spriteSet));
+
+            return new Tile(null, TileCollision.Passable);
+        }
+
+
+        private Tile LoadGemTile(int x, int y)
+        {
+            Point position = GetBounds(x, y).Center;
+            gems.Add(new Gem(this, new Vector2(position.X, position.Y)));
+
+            return new Tile(null, TileCollision.Passable);
+        }
+
+
+        public void Dispose()
+        {
+            Content.Unload();
+        }
+
+        #endregion
+
+        #region Bounds and collision
+
+
+        public TileCollision GetCollision(int x, int y)
+        {
+            if (x < 0 || x >= Width)
+                return TileCollision.Impassable;
+            if (y < 0 || y >= Height)
+                return TileCollision.Passable;
+
+            return tiles[x, y].Collision;
+        }
+
+        public Rectangle GetBounds(int x, int y)
+        {
+            return new Rectangle(x * Tile.Width, y * Tile.Height, Tile.Width, Tile.Height);
+        }
+
+        public int Width
+        {
+            get { return tiles.GetLength(0); }
+        }
+
+        public int Height
+        {
+            get { return tiles.GetLength(1); }
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Update(
+            GameTime gameTime, 
+            KeyboardState keyboardState,
+            DisplayOrientation orientation)
+        {
+            if (!Player.IsAlive || TimeRemaining == TimeSpan.Zero)
+            {
+                Player.ApplyPhysics(gameTime);
+            }
+            else if (ReachedExit)
+            {
+                int seconds = (int)Math.Round(gameTime.ElapsedGameTime.TotalSeconds * 100.0f);
+                seconds = Math.Min(seconds, (int)Math.Ceiling(TimeRemaining.TotalSeconds));
+                timeRemaining -= TimeSpan.FromSeconds(seconds);
+                score += seconds * PointsPerSecond;
+            }
+            else
+            {
+                timeRemaining -= gameTime.ElapsedGameTime;
+                Player.Update(gameTime, keyboardState, orientation);
+                UpdateGems(gameTime);
+
+                if (Player.BoundingRectangle.Top >= Height * Tile.Height)
+                    OnPlayerKilled(null);
+
+                UpdateEnemies(gameTime);
+
+
+                if (Player.IsAlive &&
+                    Player.IsOnGround &&
+                    Player.BoundingRectangle.Contains(exit))
+                {
+                    OnExitReached();
+                }
+            }
+
+            if (timeRemaining < TimeSpan.Zero)
+                timeRemaining = TimeSpan.Zero;
+        }
+
+        private void UpdateGems(GameTime gameTime)
+        {
+            for (int i = 0; i < gems.Count; ++i)
+            {
+                Gem gem = gems[i];
+
+                if (gem.BoundingCircle.Intersects(Player.BoundingRectangle))
+                {
+                    gems.RemoveAt(i--);
+                    OnGemCollected(gem, Player);
+                }
+            }
+        }
+
+
+        private void UpdateEnemies(GameTime gameTime)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.Update(gameTime);
+
+                if (enemy.BoundingRectangle.Intersects(Player.BoundingRectangle))
+                {
+                    OnPlayerKilled(enemy);
+                }
+            }
+        }
+
+        private void OnGemCollected(Gem gem, Player collectedBy)
+        {
+            score += gem.PointValue;
+
+            gem.OnCollected(collectedBy);
+        }
+
+        private void OnPlayerKilled(Enemy killedBy)
+        {
+            Player.OnKilled(killedBy);
+        }
+
+        private void OnExitReached()
+        {
+            Player.OnReachedExit();
+            exitReachedSound.Play();
+            reachedExit = true;
+        }
+
+        public void StartNewLife()
+        {
+            Player.Reset(start);
+        }
+
+        #endregion
+
+        #region Draw
+
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int i = 0; i <= EntityLayer; ++i)
+                layers[i].Draw(spriteBatch, cameraPosition);
+            spriteBatch.End();
+
+            ScrollCamera(spriteBatch.GraphicsDevice.Viewport);
+            Matrix cameraTransform = Matrix.CreateTranslation(-cameraPosition, 0.0f, 0.0f);
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, transformMatrix: cameraTransform);
+
+            DrawTiles(spriteBatch);
+
+            foreach (Gem gem in gems)
+                gem.Draw(gameTime, spriteBatch);
+
+            Player.Draw(gameTime, spriteBatch);
+
+            foreach (Enemy enemy in enemies)
+                enemy.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+
+            spriteBatch.Begin();
+            for (int i = EntityLayer + 1; i < layers.Length; ++i)
+                layers[i].Draw(spriteBatch, cameraPosition);
+            spriteBatch.End();
+        }
+
+        private void DrawTiles(SpriteBatch spriteBatch)
+        {
+            int left = (int)Math.Floor(cameraPosition / Tile.Width);
+            int right = left + spriteBatch.GraphicsDevice.Viewport.Width / Tile.Width;
+            right = Math.Min(right, Width - 1);
+            for (int y = 0; y < Height; ++y)
+            {
+                for (int x = left; x <= right; ++x)
+                {
+                    Texture2D texture = tiles[x, y].Texture;
+                    if (texture != null)
+                    {
+                        Vector2 position = new Vector2(x, y) * Tile.Size;
+                        spriteBatch.Draw(texture, position, Color.White);
+                    }
+                }
+            }
+        }
+        private void ScrollCamera(Viewport viewport)
+        {
+
+            float marginWidth = viewport.Width * 0.5f;
+            float marginLeft = cameraPosition + marginWidth;
+            float marginRight = cameraPosition + viewport.Width - marginWidth;
+
+            float cameraMovement = 0.0f;
+            if (Player.Position.X < marginLeft)
+                cameraMovement = Player.Position.X - marginLeft;
+            else if (Player.Position.X > marginRight)
+                cameraMovement = Player.Position.X - marginRight;
+
+            float maxCameraPosition = Tile.Width * Width - viewport.Width;
+            cameraPosition = MathHelper.Clamp(cameraPosition + cameraMovement, 0.0f, maxCameraPosition);
+        }
+
+        #endregion
+    }
+}
